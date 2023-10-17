daba3de75347d817e6f861bf6212edd4f9fc3c70
Obliger0 <sujalgotharwal904@gmail.com>
2023-10-17 17:17:14 +0530

    basename

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { TicTac } from './Tic Tac Toe Game/IndexPage'; 
-import { PlayersPage } from './Tic Tac Toe Game/PlayersPage';
+import { TicTac } from "./Tic Tac Toe Game/IndexPage";
+import { PlayersPage } from "./Tic Tac Toe Game/PlayersPage";
 
 function App() {
   return (
     <div className="App App-header">
-      <BrowserRouter>
+      <BrowserRouter basename="/tic-tac-toe">
         <Routes>
-          <Route path="/" element={<PlayersPage/>} />
-          <Route path="/game" element={<TicTac/>} />
+          <Route path="/" element={<PlayersPage />} />
+          <Route path="/game" element={<TicTac />} />
         </Routes>
       </BrowserRouter>
     </div>
